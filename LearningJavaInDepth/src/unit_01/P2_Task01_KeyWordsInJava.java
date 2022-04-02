aee6898babe587834e9de1544a863f5bfcea0a04
DELL <DELL@DESKTOP-5121D9G>
2022-04-02 14:15:36 +0530

    First Commit

---
+++ LearningJavaInDepth/src/unit_01/P2_Task01_KeyWordsInJava.java
@@ -0,0 +1,30 @@
+package unit_01;
+
+public class P2_Task02_KeyWordsInJava {
+	
+	public static void main(String[] args)
+	{
+		int double =12;
+		int new() {
+			System.out.println();
+			return 0;
+			
+		}
+		int a=10;
+		a=11;
+		
+		ABC int = new ABC();
+	}
+
+}
+class int{
+	void display() {
+		System.out.println("In class One ........");
+	}
+}
+
+class ABC {
+	void display() {
+		System.out.println("In class Two.......");
+	}
+}
